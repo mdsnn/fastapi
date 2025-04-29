9f9e8fbbb8ec9c790d118111c459ae7803cacef7
mwiko <mwikosinyangwe@gmail.com>
2025-04-29 16:52:42 +0200

    about

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/about")
+async def about():
+    return {"data": "This is the about page"}
+
 @app.get("/posts")
 async def get_posts():
     return {"data": "This is my posts"}
